7970d524a4293d9b8f53ced40d4e73d54ef33f72
BiohazardAlbatros <anton.klyazhev@mail.ru>
2026-04-10 00:10:03 +0300

    more mods

--- Blitz/Source/sdk_mods/Blitz/__init__.py
+++ Blitz/Source/sdk_mods/Blitz/__init__.py
@@ -1,20 +1,12 @@
-import os
 import unrealsdk
-from pathlib import Path
 from mods_base import hook, get_pc, build_mod, SETTINGS_DIR, ENGINE
 from unrealsdk.hooks import Type, Block
 from unrealsdk.unreal import UObject, WrappedStruct, BoundFunction
-from unrealsdk import logging, load_package,find_object
-from ui_utils import OptionBox, OptionBoxButton
-from typing import List, Dict, Optional, Callable
+from unrealsdk import logging,find_object
 from CharacterVault import add_custom_character_class
-import sys
 
-@hook(
-    hook_func="WillowGame.WillowGFxMoviePressStart:extContinue",
-    hook_type=Type.POST,
-)
-def Test(obj: UObject, args: WrappedStruct, ret: any, func: BoundFunction):
+@hook( hook_func="WillowGame.WillowGFxMoviePressStart:extContinue", hook_type=Type.POST )
+def ImportCharacter(obj: UObject, args: WrappedStruct, ret: any, func: BoundFunction):
     add_custom_character_class("Stormtrooper","Blitz The Stormtrooper","Blitz.Character.CharacterClass_Roland")
     Globals = find_object("GlobalsDefinition","gd_globals.General.Globals")
     Blitz1 = ENGINE.DynamicLoadObject("Blitz.Skills.Action.EndActionSkill", unrealsdk.find_class("SkillDefinition"), False)
@@ -22,14 +14,11 @@ def Test(obj: UObject, args: WrappedStruct, ret: any, func: BoundFunction):
     Globals.BasicSkills.append(Blitz1)
     Globals.BasicSkills.append(Blitz2)
 
+# third person camera setup
+#    player = get_pc().MyWillowPawn
+#    player.CameraScale = 12.0
+#    player.CameraScaleRight = 5.0
+#    player.CameraScaleUp = 2.5
 
-build_mod(
-    hooks=[Test],
-    options=[],
-    settings_file=Path(f"{SETTINGS_DIR}/Blitz.json")
-)
-
-__version__: str = "1.0.0"
-__version_info__: tuple[int, ...] = (1, 0, 0)
-
-logging.info(f"Blitz Test Loaded: {__version__}, {__version_info__}")
+build_mod(hooks=[ImportCharacter])
+logging.info(f"Character Vault: New Character Class from Blitz The Stormtrooper")
